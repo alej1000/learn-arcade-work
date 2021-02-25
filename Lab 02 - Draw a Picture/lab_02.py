bd9a720a7ab94bee26f7c8ff09ec9f67a0171ddb
alej1000 <alejandro.raboso@edu.uah.es>
2021-02-25 20:30:13 +0100

    Función nube

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -3,7 +3,7 @@ import arcade
 
 # Open up a window.
 # Set the and dimensions (width and height)
-arcade.open_window(800, 600, "Drawing Example")
+arcade.open_window(800, 600, "Mi Dibujo")
 
 # Set the background color
 arcade.set_background_color(arcade.color.AIR_SUPERIORITY_BLUE)
@@ -15,18 +15,29 @@ arcade.start_render()
 arcade.draw_lrtb_rectangle_filled(0, 800, 200, 0, arcade.color.BITTER_LIME)
 
 # Dibuja una nube
-# Derecha de la nube
-arcade.draw_circle_filled(670,490,20,arcade.color.WHITE)
-arcade.draw_circle_filled(650,470,20,arcade.color.WHITE)
-arcade.draw_circle_filled(630,450,20,arcade.color.WHITE)
-# Medio de la nube
-arcade.draw_circle_filled(640,500,20,arcade.color.WHITE)
-arcade.draw_circle_filled(620,480,20,arcade.color.WHITE)
-arcade.draw_circle_filled(600,460,20,arcade.color.WHITE)
-# Izquierda de la nube
-arcade.draw_circle_filled(610,510,20,arcade.color.WHITE)
-arcade.draw_circle_filled(590,490,20,arcade.color.WHITE)
-arcade.draw_circle_filled(570,470,20,arcade.color.WHITE)
+def dibujar_nube(x_centro,y_centro,radio,color=arcade.color.WHITE):
+    """int,int,float,object->None
+    OBJ:Dibuja una nube si le das las coordenadas de un punto
+    """
+    # Derecha de la nube
+    arcade.draw_circle_filled(x_centro, y_centro, radio, color)
+    arcade.draw_circle_filled(x_centro-20, y_centro-20, radio, color)
+    arcade.draw_circle_filled(x_centro-40, y_centro-40, radio, color)
+    # Medio de la nube
+    arcade.draw_circle_filled(x_centro-30, y_centro+10, radio, color)
+    arcade.draw_circle_filled(x_centro-50, y_centro-10, radio, color)
+    arcade.draw_circle_filled(x_centro-70, y_centro-30, radio, color)
+    # Izquierda de la nube
+    arcade.draw_circle_filled(x_centro-60, y_centro+20, radio, color)
+    arcade.draw_circle_filled(x_centro-80, y_centro, radio, color)
+    arcade.draw_circle_filled(x_centro-100, y_centro-20, radio, color)
+
+dibujar_nube(670,490,20)
+
+
+
+# Dibujar el sol
+arcade.draw_circle_filled(0,600,100,arcade.color.YELLOW)
 
 # --- Finish drawing ---
 arcade.finish_render()
